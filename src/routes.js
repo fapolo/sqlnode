b7204d57ec4e8b8d6159d047f43304ccca2e34db
Fabricio Polo <fabricio.polo@gmail.com>
2019-10-19 18:18:01 -0300

    add report

--- src/routes.js
+++ src/routes.js
@@ -3,6 +3,7 @@ const routes = express.Router();
 const UserController = require("./controllers/UserController");
 const AddressController = require("./controllers/AddressController");
 const SubjectController = require("./controllers/SubjectController");
+const ReportController = require("./controllers/ReportController");
 
 routes.get("/users", UserController.index);
 routes.post("/users", UserController.store);
@@ -13,5 +14,7 @@ routes.post("/users/:user_id/addresses", AddressController.store);
 routes.get("/users/:user_id/subjects", SubjectController.index);
 routes.post("/users/:user_id/subjects", SubjectController.store);
 routes.delete("/users/:user_id/subjects", SubjectController.delete);
+
+routes.get("/report", ReportController.show);
 
 module.exports = routes;
