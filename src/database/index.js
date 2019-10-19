6b7f0fb58bc0caf93bb0b28fc8ef2effa3f98b26
Fabricio Polo <fabricio.polo@gmail.com>
2019-10-19 16:25:53 -0300

    add index + create users

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,13 @@
 const Sequelize = require("sequelize");
 const dbConfig = require("../config/database");
 
+const User = require("../models/User");
+
 const connection = new Sequelize(
   "postgres://pzldoazr:vIkhim9E-B9zCKWe7FGVTNt9MVByLAQm@tuffi.db.elephantsql.com:5432/pzldoazr",
   dbConfig
 );
+
+User.init(connection);
 
 module.exports = connection;
--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,8 @@
 const express = require("express");
-
 const routes = express.Router();
+const UserController = require("./controllers/UserController");
 
-routes.get("/", (req, res) => res.json({ ok: true }));
+routes.get("/users", UserController.index);
+routes.post("/users", UserController.store);
 
 module.exports = routes;
