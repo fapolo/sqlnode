b5b2f954d1f0a8c35a6658aaa77b8453db5c1a49
Fabricio Polo <fabricio.polo@gmail.com>
2019-10-19 17:15:49 -0300

    add addresses index, create, associations

--- src/database/index.js
+++ src/database/index.js
@@ -2,6 +2,7 @@ const Sequelize = require("sequelize");
 const dbConfig = require("../config/database");
 
 const User = require("../models/User");
+const Address = require("../models/Address");
 
 const connection = new Sequelize(
   "postgres://pzldoazr:vIkhim9E-B9zCKWe7FGVTNt9MVByLAQm@tuffi.db.elephantsql.com:5432/pzldoazr",
@@ -9,5 +10,9 @@ const connection = new Sequelize(
 );
 
 User.init(connection);
+Address.init(connection);
+
+User.associate(connection.models);
+Address.associate(connection.models);
 
 module.exports = connection;
